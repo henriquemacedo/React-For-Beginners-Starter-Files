acf6e7dddf9cf2852990d66f6a52c61951e98fd6
Henrique Macedo <mail@henriquemacedo.pt>
2018-11-28 16:09:24 +0000

    Lesson 23 and 24
    - validation with ProTypes
    - authentication

---
+++ catch-of-the-day/src/components/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Login = props => (
+  <nav className="login">
+    <h2>Inventory Login</h2>
+    <p>Sign in to manage your store's inventory.</p>
+    <button className="twitter" onClick={() => props.authenticate("Twitter")}>
+      Log In with Twitter
+    </button>
+    <button className="github" onClick={() => props.authenticate("Github")}>
+      Log In with GitHub
+    </button>
+  </nav>
+);
+
+Login.propTypes = {
+  authenticate: PropTypes.func.isRequired
+};
+
+export default Login;
